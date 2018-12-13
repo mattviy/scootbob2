ccc992b566dc1515001cde45efe27ce4459c0ac3
mattviy <mrmhamers@gmail.com>
2018-12-13 09:36:32 +0100

    drinkermap2

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import DrinkerMap from './components/drinkers/DrinkerMap';
+// import DrinkerMap from './components/drinkers/DrinkerMap';
+
+import DrinkerMap2 from './components/drinkers/DrinkerMap2';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      origin: (41.8507300, -87.6512600),
+      destination: (41.8525800, -87.6514100)
+    };
+  };
+
   render() {
     return (
       <div className="App">
         <div style={{zIndex: '0', height: '100vh'}} >
-          <DrinkerMap />
+          <DrinkerMap2 origin={this.state.origin}
+        destination={this.state.destination} /> 
         </div>
       </div>
     );
